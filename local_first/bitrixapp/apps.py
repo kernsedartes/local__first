c2221f8391c45803cb929c1790b8ac8cbf8ec429
Pritchin Andrew <sedartes@mail.ru>
2025-07-10 15:09:26 +0300

    first commit

---
+++ local_first/bitrixapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BitrixappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bitrixapp'
---
+++ local_first/bitrixapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('deals/', views.deals, name='deals'),
+    path('create_deal/', views.create_deal, name='create_deal'),
+]
